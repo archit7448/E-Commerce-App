95f055589fd06d3d51b22b80dfd666ff59450423
Archit Singh <architsingh1064@gmail.com>
2022-03-16 19:24:11 +0530

    change structure

--- src/reducers/reducer.jsx
+++
@@ -1,8 +0,0 @@
-export const reducer = (state,action) => {
-       switch(action.type){
-         case "ADD_PRODUCTS":
-                return {...state,products:action.payload}
-         case "ADD_CATEGORIES":
-                return {...state,categories:action.payload}       
-       }
-}
