29cfbc6f3461251c70686431d2b0d5ad20c14ab9
Archit Singh <architsingh1064@gmail.com>
2022-03-18 09:09:52 +0530

    resolve branch conflict

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import { Routes ,Route } from "react-router-dom";
 import "./App.css";
 import { HomePage } from "./pages/Homepage/HomePage";
-<<<<<<< HEAD
 import {useData} from "./context/Data"
-=======
 import { ProductPage } from "./pages/Product-Page/ProductPage";
->>>>>>> dev-product
 const App = () => {
-    const {state} = useData()
-    console.log(state)
   return (
       <div>
-<<<<<<< HEAD
-      
-=======
         <Routes>
           <Route path = "/" element = {<HomePage/>}/>
           <Route path = "/products" element = {<ProductPage/>}/>
         </Routes>
->>>>>>> dev-product
       </div>
   )
 }
