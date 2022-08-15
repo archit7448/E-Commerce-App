0a504b2896aecd755ab38535eca6e07a5bc476e0
archit singh <87135461+archit7448@users.noreply.github.com>
2022-08-16 00:51:49 +0530

    add file via upload

---
+++ src/components/addressManagement/addressManager.jsx
@@ -0,0 +1,10 @@
+import { useAddress } from "../../context/address";
+
+export const AddressManager = () => {
+  const { Address, dispatch } = useAddress();
+  return(
+    <div>
+      
+    </div>
+  )
+};
